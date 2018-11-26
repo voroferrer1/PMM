4d360a95fd41817799d0c8b9e1963a68563ca36d
voroferrer <2dam.vferrer@institutoserpis.org>
2018-11-26 13:46:36 +0100

    BiciDidactico

---
+++ BiciDidactico/app/src/main/java/com/example/vorper/bicididactico/AcercaDe.java
@@ -0,0 +1,12 @@
+package com.example.vorper.bicididactico;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AcercaDe extends Activity{
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        //Hacemos visible la interfaz acercade.xml
+        setContentView(R.layout.acercade);
+    }
+}
